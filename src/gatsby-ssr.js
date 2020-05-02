1c3efbf552cb2adc33828ebe5d88be715d01e27b
Patrick Faramaz <faramazpat@gmail.com>
2020-05-02 11:37:22 +0200

    add wrap on ssr !

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -46,3 +46,18 @@ exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions = {}) => {
 
   return setPostBodyComponents(components);
 };
+
+/**
+ * wrapp app with provider for dispatch cart and customer infos
+ */
+exports.wrapRootElement = ({ element }, pluginOptions = {}) => {
+  const _options = {
+    ...{
+      version: "3.0.12",
+      locales: {},
+      defaultLang: "en",
+    },
+    ...pluginOptions,
+  };
+  return <SnipCartProvider {..._options}>{element}</SnipCartProvider>;
+};
